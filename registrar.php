6aeaa39442731212f7157d61d6237ba7458cdb5f
kose25 <kose.pablos@gmail.com>
2019-11-27 19:38:58 -0500

    Changelog: -se volvio a corregir el link del logo de la taza de cafe que mandaba al index -se puede registrar organizacion pero al hacerlo manda a una pagina en blanco -se eliminaron los mensajes y alertas de la barra superior

---
+++ registrar.php
@@ -0,0 +1,32 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+try{
+    $pdo = new PDO("mysql:host=madarme.co;dbname=estudiante_16", "estudiante_16", "LonWFNBfP1");
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+ 
+// Attempt insert query execution
+try{
+    // Create prepared statement
+    $sql = "INSERT INTO organizacion (nombre, nomusuario, password) VALUES (:nombre, :nomusuario, :password)";
+    $stmt = $pdo->prepare($sql);
+    
+    // Bind parameters to statement
+    $stmt->bindParam(':nombre', $_REQUEST['nombre']);
+    $stmt->bindParam(':nomusuario', $_REQUEST['nomusuario']);
+    $stmt->bindParam(':password', $_REQUEST['password']);
+    
+    // Execute the prepared statement
+    $stmt->execute();
+    //echo "Records inserted successfully.";
+} catch(PDOException $e){
+    die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+}
+ 
+// Close connection
+unset($pdo);
+?>
